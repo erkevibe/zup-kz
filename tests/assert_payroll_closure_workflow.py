9b780acd8d6b3e8331cb3e8ef36184dd1287189d
Baur <baur@fax.kz>
2026-09-06 19:31:58 +0500

    Add audited payroll reopening workflow

---
+++ tests/assert_payroll_closure_workflow.py
@@ -0,0 +1,15 @@
+import json
+import sys
+
+
+with open(sys.argv[1], encoding="utf-8") as source:
+    actual = json.load(source)
+
+assert actual == {
+    "reopenedAndReasonSaved": True,
+    "reclosed": True,
+    "settlementLineCount": 1,
+    "lifecycleEntryCount": 3,
+    "paymentBlocksReopen": True,
+    "blockedReasonPreserved": True,
+}
